3e1375a38c6d621709a7a19b6d9f5ec9dbbca834
Bahadir Kurban <ybahadirkurban@gmail.com>
2020-09-07 13:39:38 +0300

    Added Player Controlle Script with Jumping FNC

--- Letter Catcher/Assets/Letter.cs
+++ Letter Catcher/Assets/Letter.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Letter : MonoBehaviour
 {
+    [SerializeField] string whichLetter;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Letter Catcher/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    private bool isGrounded;
+    private Animator animator;
+
+    [SerializeField] private float jumpFactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        isGrounded = true;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpHandler();
+    }
+
+    private void JumpHandler()
+    {
+        if (Input.GetMouseButtonDown(0)&& isGrounded )
+        {
+            myRigidbody.AddForce(Vector3.up*jumpFactor,ForceMode.Impulse);
+            isGrounded = !isGrounded;
+            animator.SetTrigger("Jumping");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+            animator.SetTrigger("Running");
+        }
+    }
+}
